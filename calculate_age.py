6e13b837776101db3575488b760b967a8a6c2d4a
Rimgaudas Jurgaitis <rimgaudas.jurgaitis@gmail.com>
2024-10-03 17:25:23 +0300

    added date_of_birth missing data and calculated age with calculate_age.py

--- calculate_age.py
+++ calculate_age.py
@@ -2,22 +2,19 @@ import pandas as pd
 from datetime import datetime
 import math
 
-df = pd.read_csv("intermediate.csv")
+df = pd.read_csv("intermediate_without_missing_data.csv")
 
 def calculate_age(born):
-    if born:
-        if isinstance(born, type("string")):
-            born = datetime.fromisoformat(born)
-            today = datetime.today()
-            age_date = today - born
-            age_days = age_date.days
-            age_years = math.floor(age_days / 365.25)
-            return age_years
-    return
+    born = datetime.fromisoformat(born)
+    today = datetime.today()
+    age_date = today - born
+    age_days = age_date.days
+    age_years = math.floor(age_days / 365.25)
+    return age_years
 
 
 df["age"] = df["date_of_birth"].apply(calculate_age)
-df.insert(2, "age", df["age"])
+# df.insert(2, "age", df["age"])
 
 df = df.drop(["date_of_birth"], axis=1)
 
